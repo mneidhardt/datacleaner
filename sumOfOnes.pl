d3e70abfb99033ae9f3e6b5009746da5deb26b58
Michael Neidhardt <mneidhardt@gmail.com>
2014-11-15 14:30:46 +0100

    Finished sumOfOnes.pl - though handling enddate still missing.

--- sumOfOnes.pl
+++ sumOfOnes.pl
@@ -21,44 +21,51 @@ if (scalar(@ARGV) == 3) {
 
 my $startdatefound=0;
 my $lno=0;
-my $lno2=0;
 my $sum=0;
 my $insequence=0;
 my @sums;
 
 while (<>) {
-  ++$lno;
-  chomp;
-  next unless (/^\s*\d+\-\d+\-\d+;\d\s*$/);
-  my @parts = split(/;/);
-  ++$lno2;
+    s/\r\n/\n/g;                # Change Windows line ending to Unix.
+    chomp;
+    ++$lno;
 
-  if (scalar(@parts) != 2) {
-      die "Fandt ", scalar(@parts), " kolonner i inddata, forventede 2 i linie $lno.\n$_\n";
-  }
+    unless (/^\s*\d+\-\d+\-\d+;\d\s*$/) {
+        if ($startdatefound) {  # Stop if startdate found and line illegal.
+            last;
+        } else {                # Skip line if startdate not found and line illegal.
+            next;
+        }
+    }
 
-  unless ($startdatefound) {
-      print "$parts[0] =? $fromdate\n";
-      if ($fromdate eq $parts[0]) {
-          $startdatefound = 1;
-      }
-  }
+    my @parts = split(/;/);
 
-  if ($startdatefound) {
-      if ($parts[1] == 1) {
-          ++$sum;
-          print join(';', @parts),"\n";
-      } elsif ($parts[1] == 0) {
-          print join(';', @parts), ";$sum\n";
-          push(@sums, $sum);
-          $sum=0;
-      }
-  } else {
-      print "$_;\n";
-  }
+    if (scalar(@parts) != 2) {
+        die "Fandt ", scalar(@parts), " kolonner i inddata, forventede 2 i linie $lno.\n$_\n";
+    }
+
+    unless ($startdatefound) {
+        print "$parts[0] =? $fromdate\n";
+        if ($fromdate eq $parts[0]) {
+            $startdatefound = 1;
+        }
+    }
+
+    if ($startdatefound) {
+        if ($parts[1] == 1) {
+            ++$sum;
+            print join(';', @parts),";\n";
+        } elsif ($parts[1] == 0) {
+            print join(';', @parts), ";$sum\n";
+            push(@sums, $sum);
+            $sum=0;
+        }
+    } else {
+        print "$_;\n";
+    }
 }
 
-print "$lno lines ($lno2). ", join(" ", @sums), "\n";
+print "$lno lines. ", join(" ", @sums), "\n";
 
 
   
